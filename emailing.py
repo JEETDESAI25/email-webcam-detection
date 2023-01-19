84649b50c02fd1b68e1b95dd14a73be4120d4015
JEETDESAI25 <jeetdesai25@gmail.com>
2023-01-19 15:34:41 -0800

    Added send email with attachment

--- emailing.py
+++ emailing.py
@@ -1,2 +1,28 @@
-def send_email():
-    print("Email send")    
+import smtplib
+import imghdr
+import os
+from email.message import EmailMessage
+
+PASSWORD = os.getenv("PASSWORD")
+SENDER = "jeetdesai25@gmail.com"
+RECEIVER = "hetansheeshah@gmail.com"
+
+def send_email(image_path):
+    email_message = EmailMessage()
+    email_message["subject"] = "Someone's Here!"
+    email_message.set_content("Hey, we just saw a new person!")
+
+    with open(image_path, "rb") as file:
+        content = file.read()
+    email_message.add_attachment(content, maintype="image", subtype=imghdr.what(None, content))
+
+    gmail = smtplib.SMTP("smtp.gmail.com", 587)
+    gmail.ehlo()
+    gmail.starttls()
+    gmail.login(SENDER, PASSWORD)
+    gmail.sendmail(SENDER, RECEIVER, email_message.as_string())
+    gmail.quit()
+
+
+if __name__ == "__main__":
+    send_email(image_path="images/19.png")
